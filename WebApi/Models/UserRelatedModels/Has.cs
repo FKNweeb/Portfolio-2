ec45b2cbd40d364bf5a8ba6b3de79ab85a4bd648
DavorPavits <163551802+DavorPavits@users.noreply.github.com>
2024-10-30 12:14:39 +0100

    Create SearchHistory and Has

---
+++ WebApi/Models/UserRelatedModels/Has.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi.Models.UserRelatedModels;
+
+[Table("has")]
+public class Has
+{
+
+    public int UserId { get; set; }
+
+    
+    public int HistoryId { get; set; }
+}
---
+++ WebApi/Models/UserRelatedModels/SearchHistory.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi.Models.UserRelatedModels;
+
+[Table("search_history")]
+public class SearchHistory
+{
+    [Key]
+    [Required]
+    [Column("history_id")]
+    public int HistoryId { get; set; }
+
+    [Column("description")]
+    public string Description { get; set; }
+
+}
