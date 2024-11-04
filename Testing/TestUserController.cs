c195c0b1547f10a9ba3fb38945ed01a08ce88714
FKNweeb <yosefkarbavi@gmail.com>
2024-11-04 13:21:06 +0100

    Created testing classes

---
+++ Testing/TestUserController.cs
@@ -0,0 +1,16 @@
+using Xunit;
+
+namespace Testing;
+
+public class TestUserController
+{
+    [Fact]
+    public void TestName()
+    {
+        // Given
+    
+        // When
+    
+        // Then
+    }
+}
