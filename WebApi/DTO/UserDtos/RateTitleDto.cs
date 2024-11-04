a3c6940ef43c593007f5b56d6d027de4eaf9d8b4
DavorPavits <163551802+DavorPavits@users.noreply.github.com>
2024-11-04 13:55:48 +0100

    User Controller
    Add the rate name and rate title method.

---
+++ WebApi/DTO/UserDtos/RateTitleDto.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApi.DTO.UserDtos;
+
+public class RateTitleDto
+{
+    public string TitleId { get; set; }
+    public int Rate { get; set; }
+}
---
+++ WebApi/Models/FunctionBasedModels/SetRateTitle.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Models.FunctionBasedModels;
+
+[Keyless]
+public class SetRateTitle
+{
+    public bool IsSetRateTitle { get; set; }
+}
