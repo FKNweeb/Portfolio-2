468eb79e6702cb76690a7b97d6dd8f83bdf7b929
DavorPavits <163551802+DavorPavits@users.noreply.github.com>
2024-11-06 12:35:09 +0100

    XML Documentation text

--- WebApi/Controllers/TitleController.cs
+++ WebApi/Controllers/TitleController.cs
@@ -24,7 +24,12 @@ public class TitleController : BaseController
         _genreRepository = genreRepository;
     }
 
-
+    /// <summary>
+    /// Returns all titles found in the database  paginated.
+    /// </summary>
+    /// <param name="page">Number of Page</param>
+    /// <param name="pageSize">Items per page.</param>
+    /// <returns>Return an Ok response to the user, as well as paginated list of TItleAndPlotDto</returns>
     [HttpGet(Name = nameof(GetAllTitles))]
     public async Task<IActionResult> GetAllTitles(int page = 0, int pageSize = 25)
     {
@@ -42,6 +47,14 @@ public class TitleController : BaseController
             );
         return Ok(result);
     }
+
+    /// <summary>
+    /// Returns titles and their rating.
+    /// </summary>
+    /// <param name="queryRating">Provides the desired actions from the user. Such as the rate they are looking for or the order of the results.</param>
+    /// <param name="page">Number of Page</param>
+    /// <param name="pageSize">Items per page.></param>
+    /// <returns>Returns ok response or not found, as well as paginated list of TitleAndRating.</returns>
 
     [HttpGet("rate", Name =nameof(GetTitlesByRate))]
     public async Task<IActionResult> GetTitlesByRate([FromQuery] QueryRating queryRating, int page=0, int pageSize=25)
@@ -67,6 +80,11 @@ public class TitleController : BaseController
     }
 
 
+    /// <summary>
+    /// Returns similar titles to the title id provided based on their genres.
+    /// </summary>
+    /// <param name="id">Id is the titleId for which the users is looking for similar titles.</param>
+    /// <returns>Returns ok reposnse or not found response, with a list of all titles that match the genre.</returns>
     [HttpGet("similartitles/{id}")]
     public async Task<IActionResult> SimilarTitles([FromRoute] string id)
     {
@@ -78,6 +96,16 @@ public class TitleController : BaseController
         return Ok(SimilarTitles);
     }
 
+
+    /// <summary>
+    /// Combined search methods for one or multiple keywords.
+    /// </summary>
+    /// <param name="query">Provides the desired actions from the user. Such as the keywords they are using in order to search and the search method 
+    /// they would like to use.</param>
+    /// <param name="page">Number of Page</param>
+    /// <param name="pageSize">Items per page.</param>
+    /// <returns>Ok response if the search had a result or NotFound if the search did not have any match or bad request if the users has provided more keywords 
+    /// than allowed.</returns>
     [HttpGet("search", Name = nameof(Search))]
     public async Task<IActionResult> Search([FromQuery] QueryObject query, int page=0, int pageSize=25)
     {
@@ -148,6 +176,13 @@ public class TitleController : BaseController
     }
 
 
+    /// <summary>
+    /// Returns Titles that falls under the same category.
+    /// </summary>
+    /// <param name="page">Number of Page</param>
+    /// <param name="pageSize">Items per page.</param>
+    /// <returns>Returns Ok response, the name of the genre and all titles with primary name and title id that falls under that genre.</returns>
+
     [HttpGet("genre", Name =nameof(GetTitlesByGenre))]
     public async Task<IActionResult> GetTitlesByGenre(int page = 0, int pageSize = 25)
     {
@@ -177,8 +212,16 @@ public class TitleController : BaseController
 
     }
 
+
+    /// <summary>
+    /// Returns titles that falls under a specific genre.
+    /// </summary>
+    /// <param name="genre">The specific genre</param>
+    /// <param name="page">Number of Page</param>
+    /// <param name="pageSize">Items per Page</param>
+    /// <returns>OK Response or NotFound, and the primary title.</returns>
     [HttpGet("genre/{genre}")]
-    public async Task<IActionResult> GetTitlesForGenre([FromRoute] string genre, int page=0, int pageSize = 25)
+    public async Task<IActionResult> GetTitlesForGenre([FromRoute] string genre, int page=0, int pageSize=25)
     {
         var titles = await _genreRepository.GetTitlesBySpecificGenre(genre , page, pageSize);
 
@@ -198,7 +241,4 @@ public class TitleController : BaseController
         
     }
     
-    //TODO: Datavalidation
-    //TODO: We could also implement for every endpoint sorting 
-    //TODO: We should implement paging for every endpoint 
 }
